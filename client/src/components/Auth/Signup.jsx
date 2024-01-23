190a4e370bf7b1bb57b3ee020c583e7cfccd661c
amitsevda2303 <rohitkmt88@gmail.com>
2024-01-23 16:44:33 +0530

    working on date...

--- client/src/components/Auth/Signup.jsx
+++ client/src/components/Auth/Signup.jsx
@@ -1,52 +1,12 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import Styles from "../../styles/components/Signup.module.css";
+// import Styles from "../../styles/components/Signup.module.css";
+import Step1 from "./steps/Step1";
 
 const Signup = () => {
-  const navigate = useNavigate();
 
-  const gotoFirstPage = () => {
-    navigate("/");
-  };
+
   return (
-    <div className={Styles.container}>
-      <div className={Styles.firstSection}>
-        <div className={Styles.backbtn}>
-          <i
-            onClick={gotoFirstPage}
-            class="fa-solid fa-xmark"
-            style={{ color: "white" }}
-          ></i>
-        </div>
-        <div className={Styles.para}>
-          <h3>Step 1 of 5</h3>
-        </div>
-        <div></div>
-      </div>
-      <div className={Styles.secondSection}>
-        <span className={Styles.createHeading}>Create your account</span>
-        <div className={Styles.inputDiv}>asd</div>
-        <div className={Styles.inputDiv}>asd</div>
-        <div className={Styles.changer}>
-          <a href="/">Use Phone instead</a>
-        </div>
-        <div className={Styles.dobContainer}>
-
-        <span className={Styles.dob}>Date of birth</span>
-        <span className={Styles.dobDetail}>This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet, or something else.</span>
-
-        <div className={Styles.dobselectorDiv}>
-          <div className={Styles.selectDiv1}>month</div>
-          <div className={Styles.selectDiv2}>day</div>
-          <div className={Styles.selectDiv3}>Year</div>
-        </div>
-        </div>
-
-      </div>
-      <div className={Styles.thirdSection}>
-        <button disabled>Next</button>
-      </div>
-    </div>
+      <Step1/>
   );
 };
 
---
+++ client/src/components/Auth/steps/Step1.jsx
@@ -0,0 +1,192 @@
+import React, {  useState } from "react";
+import Styles from "../../../styles/components/steps/Step1.module.css";
+import { useNavigate } from "react-router-dom";
+
+const Step1 = () => {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [changer, setChanger] = useState("Phone");
+  const [selectedDate, setSelectedDate] = useState({
+    month: "",
+    day: "",
+    year: "",
+  });
+  const currentYear = new Date().getFullYear();
+
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+
+  const gotoFirstPage = () => {
+    navigate("/");
+  };
+  const changeType = () => {
+    if (changer === "Phone") {
+      setChanger("Email");
+      setPhone("");
+      setEmail("");
+    } else {
+      setChanger("Phone");
+      setEmail("");
+      setPhone("");
+    }
+  };
+
+  const generateDays = (month, year) => {
+    const daysInMonth = new Date(year, month, 0).getDate();
+    return Array.from({ length: daysInMonth }, (_, i) => i + 1);
+  };
+
+  const handleMonthChange = (e) => {
+    setSelectedDate({
+      ...selectedDate,
+      month: e.target.value,
+      day: "",
+    });
+  };
+
+  const handleDayChange = (e) => {
+    setSelectedDate({
+      ...selectedDate,
+      day: e.target.value,
+    });
+  };
+
+  const handleYearChange = (e) => {
+    setSelectedDate({
+      ...selectedDate,
+      year: e.target.value,
+    });
+  };
+
+
+  return (
+    <div className={Styles.container}>
+      <div className={Styles.firstSection}>
+        <div className={Styles.backbtn}>
+          <i
+            onClick={gotoFirstPage}
+            class="fa-solid fa-xmark"
+            style={{ color: "white" }}
+          ></i>
+        </div>
+        <div className={Styles.para}>
+          <h3>Step 1 of 5</h3>
+        </div>
+        <div></div>
+      </div>
+      <div className={Styles.secondSection}>
+        <div className={Styles.createHeading}>
+          <span className={Styles.createHeading}>Create your account</span>
+        </div>
+        <div className={Styles.inputDiv}>
+          <input
+            className={Styles.input1}
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            placeholder=""
+          />
+          <label className={Styles.label} htmlFor="name">
+            Name
+          </label>
+        </div>
+        <div className={Styles.inputDiv}>
+          <input
+            className={Styles.input1}
+            id="email"
+            type={changer === "Phone" ? "email" : "number"}
+            placeholder=""
+            value={changer === "Phone" ? email : phone}
+            onChange={(e) => {
+              changer === "Phone"
+                ? setEmail(e.target.value)
+                : setPhone(e.target.value);
+            }}
+          />
+          <label id="email" className={Styles.label} htmlFor="name">
+            {changer === "Phone" ? "Email" : "Phone"}
+          </label>
+        </div>
+        <div className={Styles.changer}>
+          <span onClick={changeType}>Use {changer} instead</span>
+        </div>
+        <div className={Styles.dobContainer}>
+          <span className={Styles.dob}>Date of birth</span>
+          <span className={Styles.dobDetail}>
+            This will not be shown publicly. Confirm your own age, even if this
+            account is for a business, a pet, or something else.
+          </span>
+
+          <div className={Styles.dobselectorDiv}>
+            <div className={Styles.selectDiv1}> <select
+                className={Styles.select}
+                value={selectedDate.month}
+                onChange={handleMonthChange}
+              >
+                <option value="" disabled>
+                  Month
+                </option>
+                {monthNames.map((month, index) => (
+                  <option key={index} value={index + 1}>
+                    {month}
+                  </option>
+                ))}
+              </select></div>
+            <div className={Styles.selectDiv2}> <select
+                className={Styles.select}
+                value={selectedDate.day}
+                onChange={handleDayChange}
+              >
+                <option value="" disabled>
+                  Day
+                </option>
+                {selectedDate.month &&
+                  generateDays(selectedDate.month, selectedDate.year).map((day) => (
+                    <option key={day} value={day}>
+                      {day}
+                    </option>
+                  ))}
+              </select></div>
+            <div className={Styles.selectDiv3}><select
+                className={Styles.select}
+                value={selectedDate.year}
+                onChange={handleYearChange}
+              >
+                <option value="" disabled>
+                  Year
+                </option>
+                {Array.from({ length: 100 }, (_, i) => (
+                  <option key={i + 1920} value={i + 1920}>
+                    {i + 1920}
+                  </option>
+                ))}
+              </select></div>
+          </div>
+        </div>
+      </div>
+      <div className={Styles.thirdSection}>
+        <button disabled>Next</button>
+      </div>
+    </div>
+  );
+};
+
+export default Step1;
