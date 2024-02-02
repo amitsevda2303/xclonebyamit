ce7c491cf25e0a1bcc3ec4e6c86a8197a73b8c4b
amitsevda2303 <rohitkmt88@gmail.com>
2024-02-02 22:56:41 +0530

    working on profile page.. ✨😎®

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,28 +1,27 @@
-import React, { useEffect } from 'react'
-import Styles from "../styles/pages/ProfilePage.module.css"
-import { Link, useNavigate } from 'react-router-dom'
-import AsideBar from "../components/AsideBar/AsideBar"
+import React, { useEffect } from "react";
+import Styles from "../styles/pages/ProfilePage.module.css";
+import { Link, useNavigate } from "react-router-dom";
+import AsideBar from "../components/AsideBar/AsideBar";
 
 const ProfilePage = () => {
-  const navigate = useNavigate()
-  const token = localStorage.getItem("authToken")
+  const navigate = useNavigate();
+  const token = localStorage.getItem("authToken");
 
   useEffect(() => {
     if (!token) {
-      navigate("/")      
+      navigate("/");
     }
-  }, [])
+  }, []);
   return (
     <div className={Styles.homePage}>
-      <AsideBar/>
-
+      <AsideBar />
 
       <div className={Styles.container}>
         <div className={Styles.leftContainer}>
           <div className={Styles.topdiv}>
             <div className={Styles.backbtnDiv}>
               <Link to={"/home"}>
-            <i class="fa-solid fa-arrow-left-long"></i>
+                <i class="fa-solid fa-arrow-left-long"></i>
               </Link>
             </div>
             <div className={Styles.nameDiv}>
@@ -32,32 +31,55 @@ const ProfilePage = () => {
           </div>
 
           <div className={Styles.informationDiv}>
-            
-          <div className={Styles.bannerDiv}>
-            <img src="https://c4.wallpaperflare.com/wallpaper/268/96/863/anime-clouds-artwork-sky-wallpaper-preview.jpg" alt="sorry" />
-            <div className={Styles.userPfp}>
-              <img src="https://c4.wallpaperflare.com/wallpaper/268/96/863/anime-clouds-artwork-sky-wallpaper-preview.jpg" alt="img" />
+            <div className={Styles.bannerDiv}>
+              <img
+                src="https://c4.wallpaperflare.com/wallpaper/268/96/863/anime-clouds-artwork-sky-wallpaper-preview.jpg"
+                alt="sorry"
+              />
+              <div className={Styles.userPfp}>
+                <img
+                  src="https://c4.wallpaperflare.com/wallpaper/268/96/863/anime-clouds-artwork-sky-wallpaper-preview.jpg"
+                  alt="img"
+                />
+              </div>
             </div>
-          </div>
 
-          <div className={Styles.infoDiv}>
-            <div className={Styles.editDiv}>
+            <div className={Styles.infoDiv}>
+              <div className={Styles.editDiv}>
+                <Link className={Styles.editProfileDiv}>
+                  <span> Set up profile</span>
+                </Link>
+              </div>
+              <div className={Styles.nameidContainer}>
+                <span className={Styles.Name}>Amit</span>
+                <span className={Styles.id}>@AmitSharma7424</span>
+              </div>
 
-            <Link className={Styles.editProfileDiv}>
-            <span > Edit profile</span>
-            </Link>
+              <div className={Styles.dateOfBirth}>
+                <span>
+                  <i class="fa-solid fa-calendar-days"></i>&nbsp; Joined January 2024
+                </span>
+              </div>
+              <div className={Styles.fameDiv}>
+                  <span><p>8</p> Following</span>
+                  <span><p>0</p> Followers</span>
+                </div>
             </div>
-            <div className={Styles.nameidContainer}>
-            <span className={Styles.Name}>amit</span>
-            <span className={Styles.id}>@amit98876641036</span>
-            </div>
+            <div className={Styles.lastDiv}>
+          <Link className={Styles.links}>Posts</Link>
+          <Link className={Styles.links}>Replies</Link>
+          <Link className={Styles.links}>Highlights</Link>
+          <Link className={Styles.links}>Media</Link>
+          <Link className={Styles.links}>Likes</Link>
+        </div>
           </div>
-          </div>
+          <div className={Styles.bottomContainer}></div>
         </div>
         <div className={Styles.rightContainer}>right</div>
+
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
