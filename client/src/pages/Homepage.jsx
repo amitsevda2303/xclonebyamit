2b7f0c17a9968109b485e58a1049ff763ae4e18a
amitsevda2303 <rohitkmt88@gmail.com>
2024-02-02 16:55:45 +0530

    working on profile page

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Styles from "../styles/pages/HomePage.module.css";
 import { Link, useNavigate } from "react-router-dom";
 import AsideBar from "../components/AsideBar/AsideBar";
@@ -6,12 +6,30 @@ import AsideBar from "../components/AsideBar/AsideBar";
 const Homepage = () => {
   const navigate = useNavigate();
   const token = localStorage.getItem("authToken");
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
     if (!token) {
       navigate("/");
     }
   }, []);
+
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    adjustTextareaHeight();
+  };
+
+  const calculateRows = (text) => {
+    const lines = text.split('\n');
+    return lines.length;
+  };
+  const adjustTextareaHeight = () => {
+    const textarea = document.getElementById("customTextarea");
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + "px";
+  };
+
 
   return (
     <div className={Styles.homePage}>
@@ -30,13 +48,20 @@ const Homepage = () => {
           </div>
           <div className={Styles.maincontainer}>
             <div className={Styles.textareaDiv}>
+              <div className={Styles.profileDiv}>
+              <Link to={"/profile"}>
               <i class="fa-solid fa-circle-user"></i>
+              </Link>
+              </div>
               <div className={Styles.textarea}>
-                <textarea
-                  placeholder="what is happening?"
-                  cols="70"
-                  rows="1"
-                  resize="none"
+              <textarea
+                 id="customTextarea"
+                 className={Styles.area}
+                 value={inputValue}
+                 onChange={handleInputChange}
+                 rows={calculateRows(inputValue)}
+                 style={{ whiteSpace: "pre-wrap" }}
+                 placeholder="What is happening?!"
                 ></textarea>
               </div>
             </div>
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -19,7 +19,40 @@ const ProfilePage = () => {
 
       <div className={Styles.container}>
         <div className={Styles.leftContainer}>
-        this is profile page
+          <div className={Styles.topdiv}>
+            <div className={Styles.backbtnDiv}>
+              <Link to={"/home"}>
+            <i class="fa-solid fa-arrow-left-long"></i>
+              </Link>
+            </div>
+            <div className={Styles.nameDiv}>
+              <span className={Styles.Name}>amit</span>
+              <span className={Styles.post}>0 posts</span>
+            </div>
+          </div>
+
+          <div className={Styles.informationDiv}>
+            
+          <div className={Styles.bannerDiv}>
+            <img src="https://c4.wallpaperflare.com/wallpaper/268/96/863/anime-clouds-artwork-sky-wallpaper-preview.jpg" alt="sorry" />
+            <div className={Styles.userPfp}>
+              <img src="https://c4.wallpaperflare.com/wallpaper/268/96/863/anime-clouds-artwork-sky-wallpaper-preview.jpg" alt="img" />
+            </div>
+          </div>
+
+          <div className={Styles.infoDiv}>
+            <div className={Styles.editDiv}>
+
+            <Link className={Styles.editProfileDiv}>
+            <span > Edit profile</span>
+            </Link>
+            </div>
+            <div className={Styles.nameidContainer}>
+            <span className={Styles.Name}>amit</span>
+            <span className={Styles.id}>@amit98876641036</span>
+            </div>
+          </div>
+          </div>
         </div>
         <div className={Styles.rightContainer}>right</div>
       </div>
